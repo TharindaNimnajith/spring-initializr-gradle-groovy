0e087970ed83c4672f3119648336d30ea331008d
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2023-01-08 01:36:32 +0530

    init

---
+++ src/main/groovy/com/example/springinitializrgradlegroovy/SpringInitializrGradleGroovyApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.springinitializrgradlegroovy
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringInitializrGradleGroovyApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringInitializrGradleGroovyApplication, args)
+    }
+
+}
---
+++ src/test/groovy/com/example/springinitializrgradlegroovy/SpringInitializrGradleGroovyApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.springinitializrgradlegroovy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringInitializrGradleGroovyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
